d97977accb6efec1e2d0a2e12072c7ba27df708f
Kuldeep <sharmakuldeep74420@gmail.com>
2025-02-22 11:38:28 +0530

    [Kuldeep sharma] add. Make REST Call to show Hello Mark from BridgeLabz Use GET Request Method and pass name as query parameter

--- SpringMessagingApp/src/main/java/com/hello/HelloController.java
+++ SpringMessagingApp/src/main/java/com/hello/HelloController.java
@@ -1,6 +1,7 @@
 package com.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
         public String sayHello(){
             return "Hello from BridgeLabz!";
         }
+
+    @RequestMapping("/uc2")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
 }
